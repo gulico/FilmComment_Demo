878b5badbc49aee7a3b44cbee1572c69579f4046
wxy <767293861@qq.com>
2019-01-27 22:49:47 +0800

    用户碎片美化。

--- BeanFilm/app/src/main/java/com/example/wxy/beanfilm/Fragment/MineFragment.java
+++ BeanFilm/app/src/main/java/com/example/wxy/beanfilm/Fragment/MineFragment.java
@@ -1,16 +1,30 @@
 package com.example.wxy.beanfilm.Fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.wxy.beanfilm.LoginActivity;
 import com.example.wxy.beanfilm.Model.FilmSimple;
 import com.example.wxy.beanfilm.Model.FilmSimpleLab;
 import com.example.wxy.beanfilm.R;
@@ -25,6 +39,10 @@ public class MineFragment extends Fragment {
 
     private RecyclerView mFilmSimplesRecyclerView;
     private MineFilmAdapter mAdapter;
+    private AppCompatActivity mAppCompatActivity;
+    private LinearLayout mLinearLayoutUser;
+    private TextView mTextViewUserName;
+    private ImageView mImageViewUserIcon;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,13 +53,60 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frame_mine, container, false);
+        mAppCompatActivity = ((AppCompatActivity) getActivity());
+
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.mine_toolbar);//工具栏
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)v.findViewById(R.id.mine_collapsing_toolbar);
+        mAppCompatActivity = ((AppCompatActivity) getActivity());
+
+        //将ToolBar设置为ActionBar
+        setHasOptionsMenu(true);
+        mAppCompatActivity.setSupportActionBar(toolbar);
+
+        mTextViewUserName = (TextView)v.findViewById(R.id.mine_user_neme);
+        mImageViewUserIcon = (ImageView)v.findViewById(R.id.mine_user_icon);
+        //查看本地是否有可默认登录用户
+        SharedPreferences userinfo = mAppCompatActivity.getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        String useremail = userinfo.getString("email","");
+        if(!useremail.equals("")){//有用户
+            String username = userinfo.getString("name","");
+            mTextViewUserName.setText(username);
+            //mImageViewUserIcon设置头像
+        }else{//无用户，需要登录
+            mLinearLayoutUser = (LinearLayout)v.findViewById(R.id.mine_user);
+            mLinearLayoutUser.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentToLoginActivity = new Intent(mAppCompatActivity, LoginActivity.class);
+                    startActivity(intentToLoginActivity);
+                }
+            });
+        }
 
         mFilmSimplesRecyclerView = (RecyclerView) v.findViewById(R.id.MineFilm_recycler_view);
         mFilmSimplesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
         updateUI();
 
         return v;
+    }
+
+    /*工具栏选择*/
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        mAppCompatActivity.getMenuInflater().inflate(R.menu.mine_toolbar,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(mAppCompatActivity,"点击设置按钮！",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void updateUI() {
---
+++ BeanFilm/app/src/main/java/com/example/wxy/beanfilm/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.wxy.beanfilm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        /*返回键设置*/
+        Toolbar toolbar = (Toolbar) findViewById(R.id.login_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();//返回
+            }
+        });
+
+    }
+
+}
